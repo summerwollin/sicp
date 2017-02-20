5aec47e97c81076ddd6aa9191197b85f27120f36
Summer Wollin <summer.wollin@gmail.com>
2017-02-20 14:49:45 -0700

    improve answer

--- ch1/ch1.3.scm
+++ ch1/ch1.3.scm
@@ -1,25 +1,12 @@
 ;Exercise 1.3.  Define a procedure that takes three numbers as arguments
 ;and returns the sum of the squares of the two larger numbers.
 
-(define
-  (square x)
-      (* x x)
-)
+(define (square x) (* x x))
 
-(define
-  (sum-of-squares x y)
-    (+ (square x) (square y))
-)
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
 
-(define
-  (larger x y)
-    (if (> x y) x y)
-)
-
-(define
-  (sum-squares-larger-nums x y z)
-    (if (= x (larger x y))
-      (sum-of-squares x (larger y z))
-      (sum-of-squares y (larger x z))
-    )
-)
+(define (largest-sum-squares x y z)
+  (if (>= x y)
+      (sum-of-squares x (if (>= y z) y z))
+      (sum-of-squares y (if (>= x z) x z))))
